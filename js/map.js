8eb517acf4674fe976570675863e9c25882ecb81
Jakub Červený <cervenyjakub98@gmail.com>
2020-11-30 14:21:09 +0100

    Version 0.4 *adding alerts *map in contact *dropdown categories and their coresponding pages *many more minor changes

---
+++ js/map.js
@@ -0,0 +1,13 @@
+// noinspection JSUnusedLocalSymbols
+function initMap() {
+    const home = { lat: 50.05701601351515, lng: 14.426539758567623 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: home,
+    });
+    // noinspection JSUnusedLocalSymbols
+    const marker = new google.maps.Marker({
+        position: home,
+        map: map,
+    });
+}

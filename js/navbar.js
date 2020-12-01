36b56b534e2154d07bd6895c3e62ea6b422b2c6c
Jakub Červený <cervenyjakub98@gmail.com>
2020-12-01 20:28:30 +0100

    Version 0.5 *Adding wine pages - white, sparkling, red, pink *Adding all wines page *all responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 function navColapse() {
     const x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += " responsive-nav";
     } else {
         x.className = "topnav";
     }

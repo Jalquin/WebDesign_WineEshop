0ed9285859f0b3ba46da5790a2fc3b96a99246d8
Jakub Červený <cervenyjakub98@gmail.com>
2020-12-05 20:48:19 +0100

    0.5.3 *Converting css to mobile first approach *Converting item grid from flex to grid *Minor refactor changes

--- js/alert.js
+++ js/alert.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const close = document.getElementsByClassName("closebtn");
     let i;
 
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function(){
+        close[i].onclick = function () {
             const div = this.parentElement;
             div.style.opacity = "0";
-            setTimeout(function(){ div.style.display = "none"; }, 600);
+            setTimeout(function () {
+                div.style.display = "none";
+            }, 600);
         }
     }
 }, false);
--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 // noinspection JSUnusedLocalSymbols
 function initMap() {
-    const home = { lat: 50.05701601351515, lng: 14.426539758567623 };
+    const home = {lat: 50.05701601351515, lng: 14.426539758567623};
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 16,
         center: home,

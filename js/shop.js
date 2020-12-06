6a9cde696d3b15938121149236cb45c337a1b8f2
Jakub Červený <cervenyjakub98@gmail.com>
2020-12-06 16:47:37 +0100

    0.6 *Added sorting *Collapsible sorting *Contact form in contact *Separating index.html to about_us.html - preparation for landing page on index.html *Added breadcrumbs

---
+++ js/shop.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.getElementById("myRange");
+    const output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+}, false);
+
+function sortColapse() {
+    const x = document.getElementById("Colapse");
+    if (x.className === "colapse") {
+        x.className += " colapse-open";
+    } else {
+        x.className = "colapse";
+    }
+}

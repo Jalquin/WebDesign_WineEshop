72541d279fda4a0180b43e3563a1985b0d511a0c
Jakub Červený <cervenyjakub98@gmail.com>
2020-12-12 22:51:44 +0100

    Version 0.9.1 *bug fixes

--- js/colapse.js
+++
@@ -1,8 +0,0 @@
-function infoColapse() {
-    const x = document.getElementById("Colapse");
-    if (x.className === "colapse") {
-        x.className += " colapse-open";
-    } else {
-        x.className = "colapse";
-    }
-}

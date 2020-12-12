0a36d6c835a84bfe19e6dc9eb55789b0e8ef784d
Jakub Červený <cervenyjakub98@gmail.com>
2020-12-12 22:26:24 +0100

    Version 0.9 *products WIP

--- js/shop.js
+++ js/shop.js
@@ -6,6 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
     slider.oninput = function () {
         output.innerHTML = this.value;
     }
+
+
+    const modalEle = document.querySelector(".img-modal");
+    const modalImage = document.querySelector(".img-modalImage");
+    Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+        item.addEventListener("click", event => {
+            modalEle.style.display = "block";
+            modalImage.src = event.target.src;
+        });
+    });
+    document.querySelector(".modal-img-close").addEventListener("click", () => {
+        modalEle.style.display = "none";
+    });
 }, false);
 
 function sortCollapse() {
